bbad4fc10dffba537e981e8e24358f33c8ce896f
mathieugravil <mathieu.gravil@gmail.com>
2015-03-15 15:29:04 +0100

    PHP webservice2

--- PHP/webservice/client_info.php
+++ PHP/webservice/client_info.php
@@ -6,8 +6,10 @@ if (!empty($_SERVER['HTTP_CLIENT_IP'])) {
 } else {
     $ip = $_SERVER['REMOTE_ADDR'];
 }
-
+$country_code='France';
+$city_name='Grenoble';
 $token="pk.eyJ1IjoibWF0aGlldWdyYXZpbCIsImEiOiJobnZZZG5BIn0.WtRMrGCzpuD-d3JusE0zqQ";
+
 //echo $ip ;echo"<br>";
 //echo $_SERVER['HTTP_CLIENT_IP'];echo"<br>";
 //echo $_SERVER['REMOTE_ADDR'];echo"<br>";
@@ -22,54 +24,53 @@ echo"<A HREF=\"http://www.hostip.info\"> <IMG SRC=\"http://api.hostip.info/flag.
 $temp=explode(":",$location);
 $temp2=explode("L",$temp[3]);
 $lat=trim($temp2[0]," \x00..\x1F");
-echo "X".$temp2[0]."X";echo"<br>";
 $temp2=explode("I", $temp[4]);
 $lon=trim($temp2[0]," \x00..\x1F");
 $url_precise_location="http://api.tiles.mapbox.com/v4/geocode/mapbox.places/".$lon.",".$lat.".json?access_token=".$token ;
 echo $url_precise_location;echo"<br>";
-//es/ 5.7833, 45.1833.json?access_
-$precise_location=file_get_contents($url_precise_location);
+
+//$precise_location=file_get_contents($url_precise_location);
 echo htmlspecialchars($precise_location);echo"<br>";
 
 $url_map="http://api.tiles.mapbox.com/v4/mapbox.comic/".$lon.",".$lat.",14/500x300.png?access_token=".$token ;
  
-echo"<img src=".$url_map." alt=map />";
+//echo"<img src=".$url_map." alt=map />";
 
 
-$wsdl = "http://www.webservicex.net/length.asmx?WSDL";
-
-
-$client = new SoapClient($wsdl);
- 
-$LengthValue=1;
-$fromLengthUnit='Miles';
-$toLengthUnit='Kilometers';
- 
-$res = $client->ChangeLengthUnit(array('LengthValue' => $LengthValue , 'fromLengthUnit' => $fromLengthUnit,'toLengthUnit' => $toLengthUnit ));
-//print $res->ChangeLengthUnitResult;
- 
 $wsdl_weather="http://www.webservicex.net/globalweather.asmx?WSDL";
 $client_weather = new SoapClient($wsdl_weather, array('trace' => 1));
 
-$country_code='France';
-$city_name='Grenoble';
+
 
 //$res_city=$client_weather->GetCitiesByCountry(array('CountryName'=>$country_code));
 //echo"<br>";
 // echo '<br />Reponse SOAP : '.$client_weather->__getLastResponse().'<br />';   
 //print $res_city->GetCitiesByCountryResult;
+
 $res_weather = $client_weather->GetWeather(array('CityName' => $city_name , 'CountryName'=>$country_code));
 echo"<br>";
 //print $res_weather->GetWeatherResult;
  // Affichage des requêtes et réponses SOAP (pour debug)
 // echo '<br />Requete SOAP : '.htmlspecialchars($client_weather->__getLastRequest()).'<br />';
-$xml_answer=$client_weather->__getLastResponse();
-echo '<br />Reponse SOAP : '.$xml_answer.'<br />';
-$xml=simplexml_load_string($xml_answer) or die("Error: Cannot create object");
-print_r($xml);
-    
-//phpinfo()
-print_r($res_weather)
+
+
+$xml=simplexml_load_string($res_weather->GetWeatherResult);
+
+
+if ($xml === false) {
+     echo "Failed loading XML: ";
+     foreach(libxml_get_errors() as $error) {
+         echo "<br>", $error->message;
+     }
+} else {
+echo"<table>";
+foreach ($xml->children() as $child)
+   {
+   echo "<tr><td>".$child->getName()."</td><td>" . $child . "</td></tr>";
+   }
+   echo"</table>"; // print_r($xml);
+}
+
 
 
 
