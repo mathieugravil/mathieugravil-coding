a733049b326f5a78b41300df4eef9a0220a86d5b
mathieugravil <mathieu.gravil@gmail.com>
2015-09-25 18:16:12 +0200

    powershel

---
+++ Powershell/SendLyncInstantMessage.psm1
@@ -0,0 +1,10 @@
+﻿#Start Conversation 
+$msg = New-Object "System.Collections.Generic.Dictionary[Microsoft.Lync.Model.Conversation.InstantMessageContentType,String]" 
+#Add the Message 
+$msg.Add(0,$message) 
+#choose Modality Type 
+$Modality = $Conversation.Modalities[1] 
+#Start the Dialog  
+$null = $Modality.BeginSendMessage($msg, $null, $msg) 
+#Send the Message  
+$null = $Modality.BeginSendMessage($msg, $null, $msg)
--- Powershell/VSPHERE/get_VM.ps1
+++ Powershell/VSPHERE/get_VM.ps1
@@ -1,9 +1,9 @@
 ﻿add-pssnapin VMware.VimAutomation.Core
 Connect-VIServer -Server  (read-host "Vsphere hostname") -Protocol https -User  (read-host "Set username") -Password  (read-host "Set user password" -AsSecureString)
-
+Get-Cluster
 Get-Datastore
 Get-VirtualSwitch
 Get-VMHost
 Get-VM 
-
-Disconnect-VIServer -Server * -Force
+Get-Cluster "Cluster Name" | Get-VM | Export-CSV C:\ListVMsInCluster.csv
+ Get-Cluster "CL-CLI-ORA-01" | Get-VMHost
