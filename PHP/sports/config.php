f2a943e1b3460575375869adb87406e6ff60bf32
mathieugravil <mathieu.gravil@gmail.com>
2015-12-30 14:36:22 +0100

    Update config.php
    retzaer

--- PHP/sports/config.php
+++ PHP/sports/config.php
@@ -1,8 +1,8 @@
 	<?php
 
 $db_type = 'mysqli';
-$db_host = 'sql103.byethost9.com';
-$db_name = 'b9_16875072_sports';
-$db_username = 'b9_16875072';
-$db_password = 'Spir@u38';
+$db_host = 'myhost';
+$db_name = 'sports';
+$db_username = 'b9_168sdfqsdf75072';
+$db_password = 'dfssdfqdfs';
 ?>
