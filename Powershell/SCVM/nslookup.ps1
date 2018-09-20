9ce211a35adf1a042abab140cb0c0233f1f26731
1V14713 <mathieu.gravil@total.com>
2018-09-20 17:51:22 +0200

    nslookup^

---
+++ Powershell/SCVM/nslookup.ps1
@@ -0,0 +1,20 @@
+﻿$myfile = "C:\Local\OFFSHORES.txt"
+ForEach( $line in Get-Content $myfile) {
+$ptr_state = ""
+$name = ""
+$name2 = ""
+$toto = nslookup $line
+$name=($toto[3]).split(':')[1].ToLower().replace(' ','').ToString()
+$ip=($toto[4]).split(':')[1].replace(' ','').ToString()
+$tutu = nslookup $ip
+$name2=($tutu[3]).split(':')[1].ToLower().replace(' ','').ToString()
+$ip2=($tutu[4]).split(':')[1].replace(' ','').ToString()
+if ($name2 -Contains($name) ){ 
+$ptr_sate = "OK"
+ }
+else
+{
+$ptr_state = $name2
+}
+Write-Output $name";"$ip";"$ptr_state
+}
