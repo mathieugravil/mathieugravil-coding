170028a39bc0196d199a300642e06fd92cef7d2e
1V14713 <mathieu.gravil@total.com>
2018-10-02 11:23:36 +0200

    decrypt rdg

---
+++ Powershell/decrypt_rdg.ps1
@@ -0,0 +1,33 @@
+﻿# Path to RDCMan.exe
+$RDCMan = "C:\Program Files (x86)\Microsoft\Remote Desktop Connection Manager\RDCMan.exe"
+# Path to RDG file
+$RDGFile = "C:\Local\temp\testMSDMZ.rdg"
+$TempLocation = "C:\Local\temp"
+ 
+Copy-Item $RDCMan "$TempLocation\RDCMan.dll" 
+Import-Module "$TempLocation\RDCMan.dll"
+$EncryptionSettings = New-Object -TypeName RdcMan.EncryptionSettings
+ 
+$XML = New-Object -TypeName XML
+$XML.Load($RDGFile)
+$logonCredentials = Select-XML -Xml $XML -XPath '//logonCredentials'
+
+
+$Credentials = New-Object System.Collections.Arraylist
+$logonCredentials | foreach {
+    [void]$Credentials.Add([pscustomobject]@{
+    Username = $_.Node.userName
+    Password = $(Try{[RdcMan.Encryption]::DecryptString($_.Node.password, $EncryptionSettings)}Catch{$_.Exception.InnerException.Message})
+    Domain = $_.Node.domain
+    })
+    } | Sort Username
+
+$logonCredentials2 = Select-XML -Xml $XML -XPath '//credentialsProfile'
+$logonCredentials2 | foreach {
+    [void]$Credentials.Add([pscustomobject]@{
+    Username = $_.Node.userName
+    Password = $(Try{[RdcMan.Encryption]::DecryptString($_.Node.password, $EncryptionSettings)}Catch{$_.Exception.InnerException.Message})
+    Domain = $_.Node.domain
+    })
+    } | Sort Username
+$Credentials | Sort Username
